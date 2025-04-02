a964949a83873302f8b84804f852d56862a9ac88
noasman971 <noah.kichenassamy@edu.esiee-it.fr>
2025-04-02 23:08:28 +0200

    Add sign in & upÃ

--- server/src/app/register/page.tsx
+++ server/src/app/register/page.tsx
@@ -2,68 +2,45 @@
 import Navbar from "@/components/navbar/navbar";
 import "../globals.css"
 import {Input} from "@/components/input/input";
-import {useRouter} from "next/navigation";
+import { login, signup } from './action';
 
 
 export default function Register(){
-    const router = useRouter();
 
-    async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-        e.preventDefault();
-
-        try {
-            const formData = new FormData(e.currentTarget);
-            const email = formData.get("email");
-            const password = formData.get("password");
-            const pseudo = formData.get("pseudo");
-
-            const res = await fetch("/api/auth", {
-                method: "POST",
-                headers: {
-                    "content-type": "application/json",
-                },
-                body: JSON.stringify({email, password, pseudo}),
-
-            });
-
-            if (res.status === 201) {
-                router.push("/");
-            }
-        }
-        catch (error) {
-            console.error("Error:", error);
-        }
-    }
     return(
         <>
             <Navbar/>
-            <form onSubmit={handleSubmit} className={"flex flex-col items-center justify-center h-screen bg-gradient-to-r "}>
-            <div className={"text-white flex flex-col"}>
-                <h1 className={"text-3xl font-bold   text-center mt-10"}>
-                    Inscrivez-vous pour commencer à écouter
-                </h1>
-                <label className={"text-2xl font-bold text-center mt-10"}>
-                    Email
-                <Input type={"email"}
-                       classname={"max-w-[400px] mx-auto mt-10 border-2 border-white rounded-lg p-2"}
-                       placeholder={"email"} name={"email"} isRequired={true}/>
-
-                </label>
-                <label className={"text-2xl font-bold text-center mt-10"}>
-                    Mot de passe
-                    <Input type={"password"}
+            <form className={"flex flex-col items-center justify-center h-screen bg-gradient-to-r "}>
+                <div className={"text-white flex flex-col"}>
+                    <h1 className={"text-3xl font-bold   text-center mt-10"}>
+                        Inscrivez-vous pour commencer à écouter
+                    </h1>
+                    <label className={"text-2xl font-bold text-center mt-10"}>
+                        Email
+                    <Input type={"email"}
                            classname={"max-w-[400px] mx-auto mt-10 border-2 border-white rounded-lg p-2"}
-                           placeholder={"mot de passe"} name={"password"} isRequired={true}/>
-                </label>
-                <label className={"text-2xl font-bold text-center mt-10"}>
-                    Pseudo
-                    <Input type={"text"}
-                           classname={"max-w-[400px] mx-auto mt-10 border-2 border-white rounded-lg p-2"}
-                           placeholder={"pseudo"} name={"pseudo"} isRequired={true}/>
-                </label>
-                <Input type={"submit"} classname={"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"} />
+                           placeholder={"email"} name={"email"} isRequired={true}/>
 
-            </div>
+                    </label>
+                    <label className={"text-2xl font-bold text-center mt-10"}>
+                        Mot de passe
+                        <Input type={"password"}
+                               classname={"max-w-[400px] mx-auto mt-10 border-2 border-white rounded-lg p-2"}
+                               placeholder={"mot de passe"} name={"password"} isRequired={true}/>
+                    </label>
+                    <label className={"text-2xl font-bold text-center mt-10"}>
+                        Pseudo
+                        <Input type={"text"}
+                               classname={"max-w-[400px] mx-auto mt-10 border-2 border-white rounded-lg p-2"}
+                               placeholder={"pseudo"} name={"pseudo"} isRequired={true}/>
+                    </label>
+                    <button formAction={signup}  className={"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"}>
+                        S&aposinscrire
+                    </button>
+                    <button formAction={login}  className={"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"}>
+                        Se connecter
+                    </button>
+                </div>
             </form>
 
         </>
--- server/src/model/user-model.ts
+++
@@ -1,9 +0,0 @@
-import {supabase} from "../../supabaseClient";
-
-export type Users = {
-    name: string;
-    email: string;
-    password: string;
-};
-
-export const user = supabase.model<Users>("users")
